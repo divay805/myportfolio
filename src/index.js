5d2a58b10e1f6f8defd04570409b35d7115e4a8f
divay805 <divay805@gmail.com>
2023-01-25 20:05:36 +0530

    Hello commits

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ const facebook=document.getElementById("facebook")
 const github=document.getElementById("github")
 twitter.addEventListener("click",()=>{window.location.href = 'https://twitter.com/aggarwal_divay';})
 facebook.addEventListener("click",()=>{window.location.href = 'https://www.instagram.com/divay6677/';})
-github.addEventListener("click",()=>{window.location.href = 'https://github.com/divay805';})
+github.addEventListener("click",()=>{window.location.href = 'https://github.com/divay805';});
+// Here the file got started to use in the verison of the uncle jhon 
